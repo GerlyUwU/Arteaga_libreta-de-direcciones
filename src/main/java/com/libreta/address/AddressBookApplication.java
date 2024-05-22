e296e7e815717417fc7bf457c49bd1ec6adf1158
GerlyUwU <arteaga.bernal3@gmail.com>
2024-05-22 12:23:50 -0600

    actualizamos la estructura del proyecto

--- src/main/java/com/libreta/address/AddressBookApplication.java
+++ src/main/java/com/libreta/address/AddressBookApplication.java
@@ -2,13 +2,11 @@ package com.libreta.address;
 
 import com.libreta.addressData.AddressBook;
 
-
-
 public class AddressBookApplication {
     public static void main(String[] args) {
-    //probamos la instancia de singleton (addressbook)
-    AddressBook unica = AddressBook.getInstancia();
-    unica.mostrarMensaje();
+        // probamos la instancia de singleton (addressbook)
+        AddressBook unica = AddressBook.getInstancia();
+        unica.mostrarMensaje();
     }
 
 }
