5de85c94cc3197d9b72cc1c29f253ede15a90b43
Gerly Daniel Arteaga Bernal <arteaga.bernal3@gmail.com>
2024-05-23 16:51:48 -0600

    agregamos funcionalidades al menu

--- src/main/java/com/libreta/address/AddressBookApplication.java
+++ src/main/java/com/libreta/address/AddressBookApplication.java
@@ -8,9 +8,13 @@ import com.libreta.addressData.AddressEntry;
 public class AddressBookApplication {
     public static void main(String[] args) {
         AddressBook libreta = AddressBook.getInstancia();
-        libreta.add(new AddressEntry("GERLY DANIEL",
-                "ARTEAGA BERNAL", "CALLE NAYARIT", "VERACRUZ", 96980,
-                "9231283191", "ARTEAGA.BERNAL3@GMAIL.COM"));
+        libreta.add(new AddressEntry("HECTOR LUIS",
+                "ARTEAGA BERNAL", "CALLE EL DESENGAÑO", "VERACRUZ", 96980,
+                "9612371191", "ARTEAGA.HECTOR@GMAIL.COM"));
+                
+
+        Menu.displayMenu();
+
 
    List<AddressEntry> direcciones =libreta.getAddressEntryList();
    libreta.guardarInformacion(direcciones);
--- src/main/java/com/libreta/address/Menu.java
+++ src/main/java/com/libreta/address/Menu.java
@@ -1,5 +1,15 @@
 package com.libreta.address;
 
 public class Menu {
-    
+    public static void displayMenu(){
+       System.out.println("===================================================\n"); 
+       System.out.println("Elige la opción del menu:\n");
+       System.out.println("a) Cargar de archivo");
+       System.out.println("b) Agregar");
+       System.out.println("c) Eliminar");
+       System.out.println("d) Buscar");
+       System.out.println("e) Mostrar");
+       System.out.println("f) Salir \n");
+       System.out.println("===================================================\n"); 
+    }
 }
