65de6ae25023b9b916fb40299b663ed2d3981272
Gerly Daniel Arteaga Bernal <arteaga.bernal3@gmail.com>
2024-05-26 21:32:00 -0600

    hemos agregado nuevos metodos en la clase AddressBook

--- src/main/java/com/libreta/addressData/AddressBook.java
+++ src/main/java/com/libreta/addressData/AddressBook.java
@@ -8,18 +8,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Singleton class representing an address book.
+ */
 public class AddressBook {
     private static AddressBook instancia;
+    private List<AddressEntry> addressEntryList;
 
-    List<AddressEntry> addressEntryList;
-
-    // el constructor es privado para evitar la instanciacion externa
+    /**
+     * Private constructor to avoid external instantiation.
+     */
     private AddressBook() {
-        // aqui inicializaremos variables y recursos
         addressEntryList = new ArrayList<>();
     }
 
-    // metodo publico estatico para obtener la instancia unica de la clase
+    /**
+     * Returns the unique instance of the AddressBook class.
+     *
+     * @return the singleton instance of AddressBook
+     */
     public static AddressBook getInstancia() {
         if (instancia == null) {
             instancia = new AddressBook();
@@ -27,68 +34,121 @@ public class AddressBook {
         return instancia;
     }
 
-
+    /**
+     * Gets the list of address entries.
+     *
+     * @return the list of AddressEntry objects
+     */
     public List<AddressEntry> getAddressEntryList() {
         return addressEntryList;
     }
 
+    /**
+     * Removes an address entry based on the last name.
+     *
+     * @param lastName the last name of the entry to be removed
+     */
     public void remove(String lastName) {
-
+        addressEntryList.removeIf(entry -> entry.getLastName().equalsIgnoreCase(lastName));
     }
 
-    // que hace este metodo? supongo que solo se puede usar desde esta clase
+    /**
+     * Adds a new address entry to the address book.
+     *
+     * @param addressEntry the AddressEntry object to be added
+     */
     public void add(AddressEntry addressEntry) {
         addressEntryList.add(addressEntry);
     }
 
+    /**
+     * Reads address entries from a file and populates the address book.
+     *
+     * @param filename the name of the file to read from
+     */
     public void readFromFile(String filename) {
-      try (BufferedReader lector = new BufferedReader(new FileReader(filename))) {
+        try (BufferedReader lector = new BufferedReader(new FileReader(filename))) {
             String linea;
-            int numeroContacto = 1; // Variable para enumerar los contactos
-            boolean primerNombre = true; // Flag para indicar si estamos en el primer nombre de un contacto
+            AddressEntry addressEntry = new AddressEntry();
             while ((linea = lector.readLine()) != null) {
-                if (primerNombre && !linea.isEmpty()) {
-                    System.out.println(numeroContacto + ". " + linea);
-                    primerNombre = false; // Cambiar el flag después de mostrar el primer nombre
-                } else {
-                    System.out.println(linea);
+                if (linea.startsWith("First Name: ")) {
+                    addressEntry.setFirstName(linea.substring(12));
+                } else if (linea.startsWith("Last Name: ")) {
+                    addressEntry.setLastName(linea.substring(11));
+                } else if (linea.startsWith("Street: ")) {
+                    addressEntry.setStreet(linea.substring(8));
+                } else if (linea.startsWith("State: ")) {
+                    addressEntry.setState(linea.substring(7));
+                } else if (linea.startsWith("ZIP: ")) {
+                    addressEntry.setZip(Integer.parseInt(linea.substring(5)));
+                } else if (linea.startsWith("Phone: ")) {
+                    addressEntry.setPhone(linea.substring(7));
+                } else if (linea.startsWith("Email: ")) {
+                    addressEntry.setEmail(linea.substring(7));
+                } else if (linea.isEmpty()) {
+                    addressEntryList.add(addressEntry);
+                    addressEntry = new AddressEntry();
                 }
-                if (linea.isEmpty()) {
-                    primerNombre = true; // Restaurar el flag al encontrar una línea en blanco
-                    numeroContacto++; // Incrementar el número de contacto al encontrar una línea en blanco
-                }
+            }
+            // Add the last entry if the file does not end with a blank line
+            if (addressEntry.getFirstName() != null) {
+                addressEntryList.add(addressEntry);
             }
         } catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
     }
 
-    public void find() {
-
+    /**
+     * Finds an address entry based on the last name.
+     *
+     * @param lastName the last name to search for
+     * @return the first AddressEntry that matches the last name, or null if none found
+     */
+    public AddressEntry find(String lastName) {
+        return addressEntryList.stream()
+                .filter(entry -> entry.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 
+    /**
+     * Saves the address entries to a file.
+     *
+     * @param addressEntryList the list of AddressEntry objects to be saved
+     */
     public void guardarInformacion(List<AddressEntry> addressEntryList) {
         try (BufferedWriter escritor = new BufferedWriter(new FileWriter("directorio.txt", true))) {
             for (AddressEntry addressEntry : addressEntryList) {
-                escritor.write("First Name: " + addressEntry.getFirstName());
-                escritor.newLine();
-                escritor.write("Last Name: " + addressEntry.getLastName());
-                escritor.newLine();
-                escritor.write("Street: " + addressEntry.getStreet());
-                escritor.newLine();
-                escritor.write("State: " + addressEntry.getState());
-                escritor.newLine();
-                escritor.write("ZIP: " + addressEntry.getZip());
-                escritor.newLine();
-                escritor.write("Phone: " + addressEntry.getPhone());
-                escritor.newLine();
-                escritor.write("Email: " + addressEntry.getEmail());
-                escritor.newLine();
-                escritor.newLine();  //añadimos una linea en blanco entre entradas
+                escribirEntrada(escritor, addressEntry);
             }
         } catch (IOException e) {
             System.out.println("Error al escribir en el archivo: " + e.getMessage());
         }
     }
 
+    /**
+     * Writes a single address entry to the provided BufferedWriter.
+     *
+     * @param escritor the BufferedWriter to write to
+     * @param addressEntry the AddressEntry object to be written
+     * @throws IOException if an I/O error occurs
+     */
+    private void escribirEntrada(BufferedWriter escritor, AddressEntry addressEntry) throws IOException {
+        escritor.write("First Name: " + addressEntry.getFirstName());
+        escritor.newLine();
+        escritor.write("Last Name: " + addressEntry.getLastName());
+        escritor.newLine();
+        escritor.write("Street: " + addressEntry.getStreet());
+        escritor.newLine();
+        escritor.write("State: " + addressEntry.getState());
+        escritor.newLine();
+        escritor.write("ZIP: " + addressEntry.getZip());
+        escritor.newLine();
+        escritor.write("Phone: " + addressEntry.getPhone());
+        escritor.newLine();
+        escritor.write("Email: " + addressEntry.getEmail());
+        escritor.newLine();
+        escritor.newLine();  // Add a blank line between entries
+    }
 }
--- src/main/java/com/libreta/addressData/AddressEntry.java
+++ src/main/java/com/libreta/addressData/AddressEntry.java
@@ -1,5 +1,8 @@
 package com.libreta.addressData;
 
+/**
+ * Class representing an address entry in the address book.
+ */
 public class AddressEntry {
     private String firstName;
     private String lastName;
@@ -9,12 +12,25 @@ public class AddressEntry {
     private String phone;
     private String email;
 
+    /**
+     * Default constructor.
+     */
     public AddressEntry() {
-
     }
 
+    /**
+     * Parameterized constructor to initialize all fields.
+     *
+     * @param firstName the first name of the contact
+     * @param lastName the last name of the contact
+     * @param street the street address of the contact
+     * @param state the state of the contact
+     * @param zip the ZIP code of the contact
+     * @param phone the phone number of the contact
+     * @param email the email address of the contact
+     */
     public AddressEntry(String firstName, String lastName, String street, String state, int zip, String phone,
-            String email) {
+                        String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.street = street;
@@ -24,60 +40,145 @@ public class AddressEntry {
         this.email = email;
     }
 
+    /**
+     * Sets the ZIP code.
+     *
+     * @param zip the new ZIP code
+     */
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    /**
+     * Sets the phone number.
+     *
+     * @param phone the new phone number
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Sets the email address.
+     *
+     * @param email the new email address
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name.
+     *
+     * @param firstName the new first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets the last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name.
+     *
+     * @param lastName the new last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets the street address.
+     *
+     * @return the street address
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Sets the street address.
+     *
+     * @param street the new street address
+     */
     public void setStreet(String street) {
         this.street = street;
     }
 
+    /**
+     * Gets the state.
+     *
+     * @return the state
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Sets the state.
+     *
+     * @param state the new state
+     */
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * Gets the ZIP code.
+     *
+     * @return the ZIP code
+     */
     public int getZip() {
         return zip;
     }
 
+    /**
+     * Gets the phone number.
+     *
+     * @return the phone number
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Gets the email address.
+     *
+     * @return the email address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Returns a string representation of the address entry.
+     *
+     * @return a string containing all fields of the address entry
+     */
     @Override
     public String toString() {
-        return "" +
-                "First name ='" + firstName + '\'' +
-                ", last name='" + lastName + '\'' +
-                ", street ='" + street + '\'' +
-                ", state ='" + state + '\'' +
-                ", zip =" + zip +
-                ", phone ='" + phone + '\'' +
-                ", email ='" + email + '\'';
+        return "First name ='" + firstName + '\'' +
+               ", last name='" + lastName + '\'' +
+               ", street ='" + street + '\'' +
+               ", state ='" + state + '\'' +
+               ", zip =" + zip +
+               ", phone ='" + phone + '\'' +
+               ", email ='" + email + '\'';
     }
-
 }
