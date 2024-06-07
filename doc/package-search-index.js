8125f7ddee16f7412d063952f452469de025faec
Gerly Daniel Arteaga Bernal <arteaga.bernal3@gmail.com>
2024-06-07 09:08:55 -0600

    we add the javadoc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.libreta.address"},{"l":"com.libreta.addressData"}]
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"com.libreta.addressData","l":"AddressBook"},{"p":"com.libreta.address","l":"AddressBookApplication"},{"p":"com.libreta.addressData","l":"AddressEntry"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.libreta.address","l":"Menu"}]
